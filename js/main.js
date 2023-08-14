5a96f2d3e13db1d698c68ddcc99be3fe915dd910
Pogopin <chegarakafree@gmail.com>
2023-08-14 17:21:10 +0300

    fix code

--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ const swiper = new Swiper('.swiper', {
     // slideToClickedSlide: true,
     // centeredSlides: true,
     loop: true,
-
     breakpoints: {
         640: {
             slidesPerView: 2,
@@ -25,18 +24,12 @@ const swiper = new Swiper('.swiper', {
             slidesPerView: 3,
             centeredSlides: true,
         },
-        
-
     }
   });
-
-
 let burger = document.querySelector('.header__menu-burger'),
     mobile_menu = document.querySelector('.header__menu-mobile');
-
 burger.addEventListener('click', (event)=> {
-    mobile_menu.classList.toggle('active');
-        
+    mobile_menu.classList.toggle('active');        
 });
 
 window.addEventListener('resize',function(){
@@ -44,3 +37,36 @@ window.addEventListener('resize',function(){
     //if(window.innerWidth > 1100) mobile_menu.classList.remove('active');
     //console.log(window.innerWidth);
 });
+//анимация
+let tl = gsap.timeline();
+
+let builtIcon = () => {
+    const ICON = gsap.timeline({default: {duration: 1}, repeat: -1, repeatDelay: 3});
+    ICON.to('.built__img', {rotate: 5})
+        .to('.built__img', {rotate: -5})
+        .to('.built__img', {rotate: 5})
+        .to('.built__img', {rotate: 0});
+    return ICON;
+}
+tl.from('.get__start', {opacity: 0, y: 120, duration: 1})
+  .from('.content__image', {opacity: 0, x: 120, duration: 1})
+  .add(builtIcon);
+  
+//анимация end
+let wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       300,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null,    // optional scroll container selector, otherwise use window,
+      resetAnimation: true,     // reset animation on end (default is true)
+    }
+  );
+
+wow.init();
